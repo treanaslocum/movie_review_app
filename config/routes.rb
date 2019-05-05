1602d1d333037e04ed15531e6faaa3fd5f700ab1
Treana Slocum <almightyolmec@gmail.com>
2019-05-05 14:30:36 -0700

    Add Devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+
+  # Shows urls
   get 'shows/index'
   get 'shows/show'
   get 'shows/new'
   get 'shows/edit'
   get 'shows/create'
 
+  # Home page
   get 'home/index'
 
+  # Movies urls
   get 'movies/index'
-  
+
+  # Application root url
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_180626) do
+ActiveRecord::Schema.define(version: 2019_05_05_210641) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,25 @@ ActiveRecord::Schema.define(version: 2019_05_05_180626) do
     t.string "cast"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
